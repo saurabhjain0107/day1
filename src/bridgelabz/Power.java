fa1c6f10abb89968f85410de7c0c69667cc42673
saurabh jain <luckyjain0107@gmai.com>
2022-07-03 21:22:24 +0530

    saurabh jain power of number

---
+++ src/bridgelabz/Power.java
@@ -0,0 +1,21 @@
+package bridgelabz;
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Power {
+    public static void main(String[] args) {
+        int n, p;
+        int result = 1;
+        System.out.println("enter number");
+        Scanner r = new Scanner(System.in);
+        n = r.nextInt();
+        System.out.println("enter power");
+        p = r.nextInt();
+        for (int i = 1; i <= p; i++)
+        {
+        result = n * result;
+        }
+        System.out.println("power "+result);
+    }
+}
